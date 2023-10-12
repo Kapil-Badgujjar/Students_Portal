4de99f0c0707cbc308ddf99cd8e863388c10d1b0
Kapil-Badgujjar <k.b.cool.boy99@gmail.com>
2023-10-12 17:54:32 +0530

    Bug fixed: Navbar bug fixed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,10 +2,9 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className="fixed top-0 w-full z-10 shadow-md shadow-white/30">
+    <nav className="fixed top-0 z-10 shadow-md shadow-white/30 w-screen">
         <div className='grid grid-cols-2 lg:grid-cols-3 lg:justify-items-center items-center py-2 px-5 bg-black text-white'>
-          <div className="flex gap-3 text-2xl hidden lg:block">
-            <Link href='/' className="hover:bg-[#457B9D] p-2 rounded-md">Home</Link>
+          <div className="flex gap-4 text-2xl hidden lg:block">
             <Link href='/about' className="hover:bg-[#457B9D] p-2 rounded-md">About</Link>
             <Link href='/syllabus' className="hover:bg-[#457B9D] p-2 rounded-md">Syllabus</Link>
             <Link href='/notes' className="hover:bg-[#457B9D] p-2 rounded-md">Notes</Link>
